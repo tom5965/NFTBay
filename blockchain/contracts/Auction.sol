50811e8f2f22ede3294e0908634ebfa684e763cc
Jerry <tom59651@gmail.com>
2022-12-09 18:31:30 +0900

    경매 조기종료 시 최고가 호가자가 변경되었던 부분 수정, require문에 에러 메시지 추가

--- blockchain/contracts/Auction.sol
+++ blockchain/contracts/Auction.sol
@@ -40,8 +40,8 @@ contract Auction {
     function createAuctionInstance(address tokenAddress, uint tokenId, uint startingPrice, uint auctionEndTime) external {
         //Check if token belongs to owner
         ERC721 tokenContract = ERC721(tokenAddress);
-        require(msg.sender == tokenContract.ownerOf(tokenId));
-        require(block.timestamp < auctionEndTime);
+        require(msg.sender == tokenContract.ownerOf(tokenId), "Token is not owned by account!");
+        require(block.timestamp < auctionEndTime, "Auction end time cannot be before than current time");
         auctionInstances[auctionInstanceCount] = AuctionInstance(auctionInstanceCount, msg.sender, tokenAddress, tokenId, startingPrice, msg.sender, auctionEndTime, 0, false);
         emit AuctionInstanceCreated(auctionInstanceCount);
         ownerAuctionInstanceCountMap[msg.sender]++;
@@ -50,7 +50,7 @@ contract Auction {
     }
 
     function bid(uint _auctionInstanceId, uint bidPrice) public {
-        require(_auctionInstanceId < auctionInstanceCount);
+        require(_auctionInstanceId < auctionInstanceCount, "Unknown auction id");
         AuctionInstance memory auctionInstance = auctionInstances[_auctionInstanceId];
         require(block.timestamp <= auctionInstance.auctionEndTime, "Auction Ends!");
 
@@ -79,37 +79,37 @@ contract Auction {
         emit AuctionLogCreated(_auctionInstanceId, auctionLogs[_auctionInstanceId].length - 1);
     }
 
-    function endAuction(uint _auctionInstanceId) public {
-        require(_auctionInstanceId < auctionInstanceCount);
+    function endAuction(uint _auctionInstanceId) external {
+        require(_auctionInstanceId < auctionInstanceCount, "Unknown auction id");
         AuctionInstance memory auctionInstance = auctionInstances[_auctionInstanceId];
-        require(msg.sender == auctionInstance.cosigner);
-        require(block.timestamp < auctionInstance.auctionEndTime);
+        require(msg.sender == auctionInstance.cosigner, "Token is not owned by account!");
+        require(block.timestamp < auctionInstance.auctionEndTime, "Auction already ended!");
 
-        AuctionInstance memory newAuctionInstance = AuctionInstance(auctionInstance.id, auctionInstance.cosigner, auctionInstance.tokenAddress, auctionInstance.tokenId, auctionInstance.highestBid, msg.sender, block.timestamp, 0, false);
+        AuctionInstance memory newAuctionInstance = AuctionInstance(auctionInstance.id, auctionInstance.cosigner, auctionInstance.tokenAddress, auctionInstance.tokenId, auctionInstance.highestBid, auctionInstance.highestBidder, block.timestamp, 0, false);
         auctionInstances[_auctionInstanceId] = newAuctionInstance;
     }
 
     function widthdrawFromAuctionInstance(uint _auctionInstanceId) external {
-        require(_auctionInstanceId < auctionInstanceCount);
+        require(_auctionInstanceId < auctionInstanceCount, "Unknown auction id");
         AuctionInstance memory auctionInstance = auctionInstances[_auctionInstanceId];
-        require(block.timestamp > auctionInstance.auctionEndTime);
-        require(msg.sender == auctionInstance.cosigner);
+        require(block.timestamp > auctionInstance.auctionEndTime, "Auction not ended!");
+        require(msg.sender == auctionInstance.cosigner, "Token is not owned by account!");
 
         uint received20Token = auctionInstance.received20Token;
         auctionInstance.received20Token = 0;
         auctionInstance.widthdrawFinished = true;
 
         (bool success, ) = payable(msg.sender).call{value: received20Token}("");
-        require(success);
+        require(success, "Widthdraw failed");
     }
     
     function receiveToken(uint _auctionInstanceId) external payable {
-        require(_auctionInstanceId < auctionInstanceCount);
+        require(_auctionInstanceId < auctionInstanceCount, "Unknown auction id");
         AuctionInstance memory auctionInstance = auctionInstances[_auctionInstanceId];
-        require(block.timestamp > auctionInstance.auctionEndTime);
-        require(msg.sender == auctionInstance.highestBidder);
+        require(block.timestamp > auctionInstance.auctionEndTime, "Auction not ended!");
+        require(msg.sender == auctionInstance.highestBidder, "Account is not higest bidder");
         //Check if balance is enough
-        require(msg.value >= auctionInstance.highestBid);
+        require(msg.value >= auctionInstance.highestBid, "Payment price is not correct");
         //Tranasfer bid, and get token
         auctionInstance.received20Token += msg.value;
         ERC721 tokenContract = ERC721(auctionInstance.tokenAddress);
@@ -117,7 +117,7 @@ contract Auction {
     }
 
     function getAuctionInstance(uint _auctionInstanceId) external view returns (AuctionInstance memory) {
-        require(_auctionInstanceId < auctionInstanceCount);
+        require(_auctionInstanceId < auctionInstanceCount, "Unknown auction id");
         return auctionInstances[_auctionInstanceId];
     }
 
@@ -144,7 +144,7 @@ contract Auction {
     }
 
     function getAuctionLogs(uint _auctionInstanceId) external view returns (AuctionLog[] memory) {
-        require(_auctionInstanceId < auctionInstanceCount);
+        require(_auctionInstanceId < auctionInstanceCount, "Unknown auction id");
         return auctionLogs[_auctionInstanceId];
     }
 }
